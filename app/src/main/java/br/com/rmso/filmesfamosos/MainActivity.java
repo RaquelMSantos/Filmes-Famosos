170bc26795071e5a9044c528837b0779838cf79d
Raquel Oliveira <rmso@cin.ufpe.br>
2018-05-14 21:22:52 -0300

    Create menu popular and rating

--- app/src/main/java/br/com/rmso/filmesfamosos/MainActivity.java
+++ app/src/main/java/br/com/rmso/filmesfamosos/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.rmso.filmesfamosos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemid = item.getItemId();
+
+        switch (itemid) {
+            case R.id.action_popular:
+                Toast.makeText(MainActivity.this, "Menu Popular", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.action_top_rated:
+                Toast.makeText(MainActivity.this, "Menu Rating", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
